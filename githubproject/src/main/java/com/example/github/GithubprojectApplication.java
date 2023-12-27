fa36c340911a600de343268be0c1ea04f7a86266
giwu <sungeunlee7000@gmail.com>
2023-12-27 17:47:31 +0900

    git ignore 수정(application.properties 제외 설정)

--- githubproject/src/main/java/com/example/github/GithubprojectApplication.java
+++ githubproject/src/main/java/com/example/github/GithubprojectApplication.java
@@ -8,6 +8,8 @@ public class GithubprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubprojectApplication.class, args);
+		System.out.println("github push 이후 추가합니다");
+		
 	}
 
 }
